dfe51eccb68b52bb1751e6efd39335ea1f35b88d
WeiquanWa <itvision2010@hotmail.com>
2022-07-03 18:37:01 +0800

    update run.sh

--- packaging/docker/run.sh
+++ packaging/docker/run.sh
@@ -62,20 +62,22 @@ done
 
 export HOME="$GF_PATHS_HOME"
 
-if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
-  OLDIFS=$IFS
-  IFS=','
-  for plugin in ${GF_INSTALL_PLUGINS}; do
-    IFS=$OLDIFS
-    if [[ $plugin =~ .*\;.* ]]; then
-        pluginUrl=$(echo "$plugin" | cut -d';' -f 1)
-        pluginWithoutUrl=$(echo "$plugin" | cut -d';' -f 2)
-        grafana-cli --pluginUrl "${pluginUrl}" --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${pluginWithoutUrl}
-    else
-        grafana-cli --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
-    fi
-  done
-fi
+#. Comment these lines by SEASTAR
+#. reason: plugins are installed in docker image itself
+#if [ ! -z "${GF_INSTALL_PLUGINS}" ]; then
+#  OLDIFS=$IFS
+#   IFS=','
+#   for plugin in ${GF_INSTALL_PLUGINS}; do
+#     IFS=$OLDIFS
+#     if [[ $plugin =~ .*\;.* ]]; then
+#         pluginUrl=$(echo "$plugin" | cut -d';' -f 1)
+#         pluginWithoutUrl=$(echo "$plugin" | cut -d';' -f 2)
+#         grafana-cli --pluginUrl "${pluginUrl}" --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${pluginWithoutUrl}
+#     else
+#         grafana-cli --pluginsDir "${GF_PATHS_PLUGINS}" plugins install ${plugin}
+#     fi
+#   done
+# fi
 
 exec grafana-server                                         \
   --homepath="$GF_PATHS_HOME"                               \
